5a67ddaf64639a31e34ea1cee62df611dd446451
yiding215 <42981504+yiding215@users.noreply.github.com>
2026-01-21 17:46:34 +0100

    升级版本号1.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.innerken.aadenprinterx"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.0.19"
+        versionCode = 21
+        versionName = "1.0.21"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
